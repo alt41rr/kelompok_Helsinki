b87c54bd38bcc4dcb6afb0758cce38aa12c7bb92
Rionaldo16 <136539297+Rionaldo16@users.noreply.github.com>
2023-06-14 09:39:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+// Destructure Array & objek
+
+// let numbers = [1, 2, 3, 4, 5];
+// Destructuring semua
+// let  [num1, num2, num3, num4, num5] = numbers;
+
+// Destructuring beberapa
+// let [num1, num3, num5] = numbers;
+
+// console.log(num1);
+
+// let john = {
+//     fullName: "John Doe",
+//     age:33
+// };
+
+// const { fullName, age } = john;
+
+// console.log(fullName);
+// console.log(age);
+
+// // Destructuring pada parameter fungsi
+// const greetings =({ fullName, age }) => {
+//     return `hello my name is ${fullName}, im ${age} years old`;
+// }
+// console.log(greetings(john));
+
+
+//=====================================
+// Module
+//=====================================
+
+// import { nama, angka, john} from "./utils.js";
+
+// console.log(nama);
+// console.log(angka);
+// console.log(john);
+
+
+//=====================
+// asyncrounous javascript
+//====================
+
+console.log("proses 1");
+console.log("proses 2");
+console.log("proses 3");
+setTimeout(() => {
+    console.log
+})
+
+
+
