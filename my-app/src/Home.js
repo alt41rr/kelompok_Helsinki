f68ada0d9676757e1c2353b8bf40b68839d10716
Rionaldo16 <rionaldosiaman@gmail.com>
2023-06-21 08:36:22 +0800

    menambahkan file

---
+++ my-app/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './Search';
+import CardInfo from './CardInfo';
+
+function Home() {
+    return (
+        <div>
+            <Search />
+            <CardInfo />
+        </div>
+    );
+}
+
+export default Home;
