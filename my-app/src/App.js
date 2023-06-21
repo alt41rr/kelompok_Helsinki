540e6ad440b039cc747cbd858969a89974cc00a7
Rionaldo16 <rionaldosiaman@gmail.com>
2023-06-21 08:35:51 +0800

    menambahkan home dan search

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Evolution from './Evolution';
+import TypeEffects from './TypeEffects';
+import Logo from './Logo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function CardInfo() {
+    return (
+        <div>
+            <h2>Card Info</h2>
+            <Evolution />
+            <TypeEffects />
+            <Logo />
+        </div>
+    );
 }
 
-export default App;
+export default CardInfo;
